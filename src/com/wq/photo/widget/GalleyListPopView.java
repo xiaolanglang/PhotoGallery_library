22a4b2a988d201d2ffa635af7ece24f2d663da94
xiaolanglang <1712877559@qq.com>
2016-02-16 21:57:10 +0800

    加入照片查看库，这时一个开源的项目中的
    Signed-off-by: xiaolanglang <1712877559@qq.com>

---
+++ src/com/wq/photo/widget/GalleyListPopView.java
@@ -0,0 +1,11 @@
+package com.wq.photo.widget;
+
+import android.content.Context;
+
+/**
+ * Created by wangqiong on 15/3/30.
+ */
+public class GalleyListPopView {
+	public GalleyListPopView(Context context) {
+	}
+}
